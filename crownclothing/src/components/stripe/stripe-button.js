e9bb322cc3c72d60a8a4364850ed5683107e684f
Soumya Chakraborty <techno.soumya@gmail.com>
2021-09-11 15:00:00 +0530

    Payment Gateway integration and title and favicon modification

---
+++ crownclothing/src/components/stripe/stripe-button.js
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JYQOrSGanQ2vugDpHbILF5QJc72k8bqAe0Q1nUSXk4N78KTrtG18A1go9VL6LIguPgv6VX8JytMEIE022VpDSbI00WvuGh9Jn";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- crownclothing/src/pages/checkout/checkout.js
+++ crownclothing/src/pages/checkout/checkout.js
@@ -5,6 +5,7 @@ import { createStructuredSelector } from "reselect";
 
 import { selectCartTotal, selectCartItems } from "../../redux/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item";
+import StripeCheckoutButton from "../../components/stripe/stripe-button";
 const Checkout = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
@@ -17,6 +18,9 @@ const Checkout = ({ cartItems, total }) => (
       <div className="header-block">
         <span>Quantity</span>
       </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
 
       <div className="header-block">
         <span>Remove</span>
@@ -26,6 +30,12 @@ const Checkout = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the folling credit card for payments*
+      <br />
+      4242 4242 4242 4242 -Exp: 01/22 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
